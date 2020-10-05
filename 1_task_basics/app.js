17736950f97dbd20d3f7873dc16c94b64f90be58
Testi Käyttäjä <testikayttaja@Testi-MBP.home>
2020-10-05 21:46:56 +0300

    school 1st period exercises

---
+++ 1_task_basics/app.js
@@ -0,0 +1,9 @@
+var a = 3;
+var b = 2;
+
+if (a > b) {
+    console.log('a is greater than b');
+    console.log(a + ' is greater than ' + b);
+}
+
+// concatenation- concat
---
+++ 7_task_globaltrade/script.js
@@ -0,0 +1,3 @@
+function hideTextarea() {
+    document.getElementById('textarea');
+}
